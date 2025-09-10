a92089e06293a8e7b062699729c0d1b895d193a9
Ivan <zhukekIvan@yandex.ru>
2025-09-10 16:50:46 +0300

    feat: add pg connect

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -116,7 +116,7 @@ func TestNewRouter(t *testing.T) {
 
 	var initData []byte
 	storage, _ := models.NewStorage(initData)
-	server := httptest.NewServer(NewRouter(storage))
+	server := httptest.NewServer(NewRouter(storage, nil))
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
