e71a0ff1a067f1bf7943ed718e0aca9579482800
Ivan <zhukekIvan@yandex.ru>
2025-09-18 16:40:14 +0300

    fix

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -51,7 +51,7 @@ func updatev1(res http.ResponseWriter, req *http.Request, storage repository.Rep
 }
 
 func updatev2(res http.ResponseWriter, req *http.Request, storage repository.Repository) {
-	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	var metric models.Metrics
 
